da833176788b7fda65b6ecefdab7571d7220d5f4
atzitz-amos <amos.atzitz@gmail.com>
2020-03-14 09:36:32 +0100

    Ajouter le défilement

--- lettersMAJ.py
+++ lettersMAJ.py
@@ -1,10 +1,13 @@
 
+from scipy.signal.lti_conversion import ss2tf
 import math
+import threading
+import time
 import turtle as t
 
 tu = t.Turtle(visible=True)
 tu.shape('turtle')
-tu.speed(0)
+tu.speed(3)
 """
 x,y
 
@@ -290,13 +293,73 @@ def Z(x,y,d):
     tu.fd(d / 2)
 
 
+def onOnePlace(letters,x,y,d,espacement=10):
+    for l in letters:
+        tu.clear()
+        l(x,y,d)
+        time.sleep(espacement / 100)
+
+
+class DefilThread():
+
+    def __init__(self,letters,size,x,y,diameter,interval,delay):
+        super().__init__()
+
+        self.delay = delay
+        self.letters = letters
+        self.size = size
+        self.x = x
+        self.y = y
+        self.diameter = diameter
+        self.interval = interval
+    
+    def run(self):
+        sx,sy = self.x,self.y
+        for i,l in enumerate(self.letters):
+            tu.clear()
+            letters = self.letters[:i]
+            x,y = sx,sy
+            l(sx,sy,self.diameter)
+            for letter in reversed(letters):
+                x -= self.interval
+                if x >= self.size:
+                    break
+                letter(x,y,self.diameter)
+                time.sleep(self.delay / 100)
+        sx -= self.interval
+        while True:
+            tu.clear()
+            for l in self.letters:
+                l(sx,sy,self.diameter)
+                sx -= self.interval
+            print(sx,self.size)
+            if abs(sx) >= self.size:
+                tu.clear()
+                break
+
+
+def defil(letters,screensize,start=None,diameter=100,interval=None,delay=10):
+    if not interval:
+        interval = diameter / 2 + 10
+    if not start:
+        start = (screensize - screensize / 4,0)
+    DefilThread(letters,screensize,*start,diameter,interval,delay).run()
+
 # var = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
-var = [B,O,N,N,E,N,U,I,T]
+
+
+"""var = [B,O,N,N,E,N,U,I,T]
 
 start = -550
 step = 100
 for v in var:
     v(start,10,100)
-    start += step
+    start += step"""
+
+
+tu.speed(10)
+
+
+defil([A,S,X],400,interval=60)
 tu.ht()
 tu.screen.mainloop()
